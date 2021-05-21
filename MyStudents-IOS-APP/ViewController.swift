1c43264b07761afc44e1c66242d063f1702d8a1d
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-05-21 14:31:27 +0100

    Initial Commit

---
+++ MyStudents-IOS-APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyStudents-IOS-APP
+//
+//  Created by Mehdi Benrefad on 21/05/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
