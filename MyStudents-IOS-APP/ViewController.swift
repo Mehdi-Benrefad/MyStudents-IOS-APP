4064bc59a9978a02ac8cc595527b9714acf8e108
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-05-23 18:24:09 +0100

    edit student

--- MyStudents-IOS-APP/ViewController.swift
+++ MyStudents-IOS-APP/ViewController.swift
@@ -20,16 +20,18 @@ class ViewController: UIViewController , UITableViewDataSource , UITableViewDele
         // Do any additional setup after loading the view.
     }
     
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         students = studentsInSections
         studentsTableView.reloadData()
-        
     }
+    
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return students.count
     }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         for i in 0..<students.count{
@@ -48,12 +50,13 @@ class ViewController: UIViewController , UITableViewDataSource , UITableViewDele
         cell.detailTextLabel?.text = student[indexPath.row].lastname
         cell.imageView?.image = UIImage(named: "profile")
         return cell
-       
     }
+    
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "CLASSE NUMERO: \(section + 1)"
     }
+    
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete{
@@ -62,5 +65,51 @@ class ViewController: UIViewController , UITableViewDataSource , UITableViewDele
             studentsTableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
-}
+    
+    
+    //edit a student
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var studentToEdit = studentsInSections[indexPath.section][indexPath.row]
+        var alertToEdit = UIAlertController(title: "Edit Student", message: "Enter the new fiestname and the new lastname", preferredStyle:.alert)
+        alertToEdit.addTextField()
+        alertToEdit.addTextField()
+        
+        //recuperation
+        var nom = alertToEdit.textFields![0]
+        var prenom = alertToEdit.textFields![1]
+        
+        //remplir les champs par les anciennes valeurs
+        nom.text = studentToEdit.firstname
+        prenom.text = studentToEdit.lastname
+        
+        //on met un ecouteur sur le bouton save
+        let saveButton = UIAlertAction(title: "save" , style: .default){ (action) in
+            
+        //on remliot le contact par les nouvelles valeurs
+        studentToEdit.firstname=alertToEdit.textFields![0].text!
+        studentToEdit.lastname=alertToEdit.textFields![1].text!
+        
+        //on enregistre les nouvelles donnees
+            studentsInSections[indexPath.section][indexPath.row].firstname = studentToEdit.firstname
+            studentsInSections[indexPath.section][indexPath.row].lastname = studentToEdit.lastname
+
+            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let newViewController = storyBoard.instantiateViewController(withIdentifier: "HomeViewController") as! UIViewController
+            newViewController.modalPresentationStyle = .fullScreen
+            self.present(newViewController, animated: true, completion: nil)
+            
+        }
+        
+            
+        //ajouter une action a l'alerte
+       alertToEdit.addAction(saveButton)
+       //lancer l'alerte
+       self.present(alertToEdit, animated: true, completion: nil)
+       
+        
+        }
+        
+       
+    }
+    
 
